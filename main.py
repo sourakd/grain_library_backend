fb43f3f7acecb53a16eb85111ca1908ad69f68c4
sourakd <dhar_sourav@ymail.com>
2024-05-22 17:54:04 +0530

    Project setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import create_app
 
-app = create_app(config_name='development')
+app = [create_app(config_name='development'), create_app(config_name='production')]
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    # Check if the host is localhost
+    app[0].run(host="127.0.0.1", port=8000, debug=True)
