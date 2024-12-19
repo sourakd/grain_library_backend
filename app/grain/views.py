b80dee9d35db46468e911dce7393ce5a18ddf91d
sourakd <dhar_sourav@ymail.com>
2024-12-19 13:35:09 +0530

    grain details and status_change

--- app/grain/views.py
+++ app/grain/views.py
@@ -1,5 +1,6 @@
 import datetime as dt
 
+from bson import ObjectId
 from flask import Blueprint, make_response, jsonify, request
 from flask.views import MethodView
 from flask_cors import cross_origin
@@ -20,7 +21,7 @@ class AddGrain(MethodView):
             if db is not None:
                 db1 = db["grain"]
                 data = request.get_json()
-                grain = data['grain']
+                grain = data['grain'].lower()
 
                 if grain:
                     # Validate the data using the schema
@@ -34,7 +35,7 @@ class AddGrain(MethodView):
                     except ValidationError as err:
                         response = {"message": err.messages, "status": "val_error"}
                         stop_and_check_mongo_status(conn)
-                        return make_response(jsonify(response)), 200
+                        return make_response(jsonify(response)), 400
 
                     else:
                         # Update the updated_at field
@@ -59,18 +60,18 @@ class AddGrain(MethodView):
                 else:
                     response = {"status": 'val_error', "message": {"Details": ["Please enter all details"]}}
                     stop_and_check_mongo_status(conn)
-                    return make_response(jsonify(response)), 200
+                    return make_response(jsonify(response)), 400
             else:
                 response = {"status": 'val_error', "message": "Database connection failed"}
                 stop_and_check_mongo_status(conn)
-                return make_response(jsonify(response)), 200
+                return make_response(jsonify(response)), 400
 
         except Exception as e:
             import traceback
             traceback.print_exc()
             response = {"status": 'val_error', "message": f'{str(e)}'}
             stop_and_check_mongo_status(conn)
-            return make_response(jsonify(response)), 200
+            return make_response(jsonify(response)), 400
 
     class AddGrainVariant(MethodView):
         @cross_origin(supports_credentials=True)
@@ -81,7 +82,7 @@ class AddGrain(MethodView):
                 if db is not None:
                     db1 = db["grain"]
                     data = request.get_json()
-                    grain = data['grain']
+                    grain = data['grain'].lower()
                     grain_variant = data['grain_variant']
 
                     if grain and grain_variant:
@@ -96,7 +97,7 @@ class AddGrain(MethodView):
                         except ValidationError as err:
                             response = {"message": err.messages, "status": "val_error"}
                             stop_and_check_mongo_status(conn)
-                            return make_response(jsonify(response)), 200
+                            return make_response(jsonify(response)), 400
 
                         else:
                             # Update the updated_at field
@@ -121,18 +122,18 @@ class AddGrain(MethodView):
                     else:
                         response = {"status": 'val_error', "message": {"Details": ["Please enter all details"]}}
                         stop_and_check_mongo_status(conn)
-                        return make_response(jsonify(response)), 200
+                        return make_response(jsonify(response)), 400
                 else:
                     response = {"status": 'val_error', "message": "Database connection failed"}
                     stop_and_check_mongo_status(conn)
-                    return make_response(jsonify(response)), 200
+                    return make_response(jsonify(response)), 400
 
             except Exception as e:
                 import traceback
                 traceback.print_exc()
                 response = {"status": 'val_error', "message": f'{str(e)}'}
                 stop_and_check_mongo_status(conn)
-                return make_response(jsonify(response)), 200
+                return make_response(jsonify(response)), 400
 
 
 class FetchGrain(MethodView):
@@ -159,19 +160,19 @@ class FetchGrain(MethodView):
                 else:
                     response = {"status": 'val_error', "message": {"Grain": ["Please add a grain first"]}}
                     stop_and_check_mongo_status(conn)
-                    return make_response(jsonify(response)), 200
+                    return make_response(jsonify(response)), 400
 
             else:
                 response = {"status": 'val_error', "message": {"DB": ["Database connection failed"]}}
                 stop_and_check_mongo_status(conn)
-                return make_response(jsonify(response)), 200
+                return make_response(jsonify(response)), 400
 
         except Exception as e:
             import traceback
             traceback.print_exc()
             response = {"status": 'val_error', "message": f'{str(e)}'}
             stop_and_check_mongo_status(conn)
-            return make_response(jsonify(response)), 200
+            return make_response(jsonify(response)), 400
 
 
 # class FetchGrain(MethodView):
@@ -242,32 +243,120 @@ class FetchGrainVariant(MethodView):
                         response = {"status": 'val_error', "message": {"Grain_variant": ["Please add a grain variant "
                                                                                          "first"]}}
                         stop_and_check_mongo_status(conn)
-                        return make_response(jsonify(response)), 200
+                        return make_response(jsonify(response)), 400
 
                 else:
                     response = {"status": 'val_error', "message": {"Details": ["Please enter all details"]}}
                     stop_and_check_mongo_status(conn)
-                    return make_response(jsonify(response)), 200
+                    return make_response(jsonify(response)), 400
 
             else:
                 response = {"status": 'val_error', "message": "Database connection failed"}
                 stop_and_check_mongo_status(conn)
-                return make_response(jsonify(response)), 200
+                return make_response(jsonify(response)), 400
 
         except Exception as e:
             import traceback
             traceback.print_exc()
             response = {"status": 'val_error', "message": f'{str(e)}'}
             stop_and_check_mongo_status(conn)
-            return make_response(jsonify(response)), 200
+            return make_response(jsonify(response)), 400
+
+
+class FetchGDetails(MethodView):
+    @cross_origin(supports_credentials=True)
+    def post(self):
+        try:
+            start_and_check_mongo()
+            db = database_connect_mongo()
+            if db is not None:
+                db1 = db["grain"]
+                data = request.get_json()
+                g_id = data["g_id"]
+                type_id = data["type_id"]
+
+                if g_id and type_id:
+                    find_details = db1.find_one(
+                        {"_id": ObjectId(g_id), "status": {"$in": ["active", "inactive"]}, "type_id": type_id})
+
+                    if find_details:
+                        find_details["_id"] = str(find_details["_id"])
+                        response = {"status": "success", "data": find_details, "message": "Grain details fetched "
+                                                                                          "successfully"}
+                        stop_and_check_mongo_status(conn)
+                        return make_response(jsonify(response)), 200
+
+                    else:
+                        response = {"status": 'val_error', "message": {"Details": ["Grain not found"]}}
+                        stop_and_check_mongo_status(conn)
+                        return make_response(jsonify(response)), 400
+
+                else:
+                    response = {"status": 'val_error', "message": {"Details": ["Please enter all details"]}}
+                    stop_and_check_mongo_status(conn)
+                    return make_response(jsonify(response)), 400
+
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            response = {"status": 'val_error', "message": f'{str(e)}'}
+            stop_and_check_mongo_status(conn)
+            return make_response(jsonify(response)), 400
+
+
+class GStatusChange(MethodView):
+    @cross_origin(supports_credentials=True)
+    def post(self):
+        try:
+            start_and_check_mongo()
+            db = database_connect_mongo()
+            if db is not None:
+                db1 = db["grain"]
+                data = request.get_json()
+                g_status = data["g_status"].lower()
+                gs_id = data["gs_id"]
+                type_id = data["type_id"]
+
+                if g_status and gs_id and type_id:
+                    current_status = db1.find_one({"_id": ObjectId(gs_id), "type_id": type_id}, {"status": 1})
+                    if current_status["status"] == g_status:
+                        response = {"status": 'val_error', "message": {"Details": [f"Grain is already {g_status}"]}}
+                        stop_and_check_mongo_status(conn)
+                        return make_response(jsonify(response)), 200
+
+                    else:
+                        db1.update_one({"_id": ObjectId(gs_id), "type_id": type_id}, {"$set": {"status": g_status}})
+                        response = {"status": "success", "message": f"Grain {g_status} successfully"}
+                        stop_and_check_mongo_status(conn)
+                        return make_response(jsonify(response)), 400
+                else:
+                    response = {"status": 'val_error', "message": {"Details": ["Please enter all details"]}}
+                    stop_and_check_mongo_status(conn)
+                    return make_response(jsonify(response)), 400
+
+            else:
+                response = {"status": 'val_error', "message": "Database connection failed"}
+                stop_and_check_mongo_status(conn)
+                return make_response(jsonify(response)), 400
+
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            response = {"status": 'val_error', "message": f'{str(e)}'}
+            stop_and_check_mongo_status(conn)
+            return make_response(jsonify(response)), 400
 
 
 grain_add_view = AddGrain.as_view('grain_add_view')
 grain_variant_add_view = AddGrain.AddGrainVariant.as_view('grain_variant_add_view')
 grain_fetch_view = FetchGrain.as_view('grain_fetch_view')
 grain_variant_fetch_view = FetchGrainVariant.as_view('grain_fetch_variant_view')
+g_status = GStatusChange.as_view('g_status')
+g_details = FetchGDetails.as_view('g_details')
 
 grain_add.add_url_rule('/grain/add_grain', view_func=grain_add_view, methods=['POST'])
 grain_add.add_url_rule('/grain/add_grain_variant', view_func=grain_variant_add_view, methods=['POST'])
 grain_add.add_url_rule('/grain/fetch_grain', view_func=grain_fetch_view, methods=['POST'])
 grain_add.add_url_rule('/grain/fetch_grain_variant', view_func=grain_variant_fetch_view, methods=['POST'])
+grain_add.add_url_rule('/grain/g_status', view_func=g_status, methods=['POST'])
+grain_add.add_url_rule('/grain/g_details', view_func=g_details, methods=['POST'])
