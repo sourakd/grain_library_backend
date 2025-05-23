f59e21b0591ef6be7570516082029af6a6905baa
sourakd <dhar_sourav@ymail.com>
2025-05-23 14:36:51 +0530

    new server for production

--- app/super_employee/views.py
+++ app/super_employee/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import jwt
 import pytz
+from bson import ObjectId
 from flask import Blueprint, make_response, jsonify, request, current_app
 from flask.views import MethodView
 from flask_cors import cross_origin
@@ -46,8 +47,7 @@ class SuperEmployeeRegistration(MethodView):
                             "created_at": dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "updated_at": None,
                             "employee_name": employee_name, "email_id": email_id,
                             "address": address, "id_proof": id_proof, "id_no": id_no,
-                            "phone_number": phone_number, "assign": "false",
-                            "profile_pic": profile_pic}
+                            "phone_number": phone_number, "profile_pic": profile_pic}
 
                     # Validate the data using the schema
                     try:
@@ -60,13 +60,15 @@ class SuperEmployeeRegistration(MethodView):
                     else:
                         # Update the updated_at field
                         validated_data["created_at"] = str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                        validated_data["loc_assign"] = "false"
+                        validated_data["type_id"] = "super_admin"
 
                         s3_config = S3Config()
                         bucket_status, total_files, all_folders = s3_config.connect_to_s3()
                         s3_uploader = S3Uploader(s3_config)
                         file_url = s3_uploader.upload_file(profile_pic)
 
-                        if s3_uploader.check_existing_file(file_url['file_url']):
+                        if s3_uploader.check_existing_file(file_url['file_url'], type_id):
                             response = {"message": "File already exist", "status": "val_error"}
                             stop_and_check_mongo_status(conn)
                             return make_response(jsonify(response)), 400
@@ -122,7 +124,8 @@ class AddSuperLocation(MethodView):
                     # Validate the data using the schema
                     data = {"status": "active",
                             "created_at": dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "updated_at": None,
-                            "password": password, "location": location, "email_id": email_id, "emp_assign": "false"}
+                            "password": password, "location": location, "email_id": email_id,
+                            "emp_assign": "false"}
 
                     # Validate the data using the schema
                     try:
@@ -133,6 +136,7 @@ class AddSuperLocation(MethodView):
                         return make_response(jsonify(response)), 400
 
                     else:
+
                         # Hash the password
                         validated_data["password"] = pbkdf2_sha256.hash(validated_data["password"])
 
@@ -140,7 +144,7 @@ class AddSuperLocation(MethodView):
                         validated_data["created_at"] = str(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
                         # Add type
-                        validated_data["type_id"] = "country"
+                        validated_data["type_id"] = "super_location"
 
                     db1.insert_one(validated_data)
 
@@ -185,6 +189,7 @@ class SuperLogin(MethodView):
             if db is not None:
                 db1 = db["super_location"]
                 db2 = db["login_token"]
+                db3 = db["super_employee"]
                 db2.create_index("expired_at", expireAfterSeconds=0)
                 data = request.get_json()
                 email_id = data["email_id"].lower()
@@ -205,29 +210,43 @@ class SuperLogin(MethodView):
                     else:
                         # simpler version to do check {"email_id": {"$regex": "^" + value + "$", "$options": "i"}}{
                         # "email_id": {"$regex": "^" + value + "$", "$options": "i"}}
-                        user = db1.find_one(
+                        location = db1.find_one(
                             {"$and": [{'email_id': re.compile("^" + re.escape(email_id) + "$", re.IGNORECASE)}],
                              "status": "active"})
 
+                        emp_id = location.get("s_e_id", "Employee not assign")
+
+                        employee = db3.find_one({"_id": ObjectId(emp_id), "status": "active"})
+
+                        if not employee:
+                            response = {"status": 'val_error',
+                                        "message": {"Details": ["Employee not found"]}}
+                            stop_and_check_mongo_status(conn)
+                            return make_response(jsonify(response)), 400
+
                         # Delete the previous token if exists(only single token)
-                        db2.delete_many({"e_id": str(user["_id"])})
+                        db2.delete_many({"e_id": str(location["_id"])})
 
                         s_token = jwt.encode(
-                            {'_id': str(user["_id"]),
+                            {'_id': str(location["_id"]),
                              'exp': datetime.now(pytz.utc) + dt.timedelta(days=7)},
                             current_app.config['SECRET_KEY'], algorithm="HS256")
 
-                        del user["password"]
-                        user["_id"] = str(user["_id"])
-                        user["token"] = s_token
+                        del location["password"]
+                        location["_id"] = str(location["_id"])
+                        location["token"] = s_token
+                        location["profile_pic"] = employee["profile_pic"]["file_url"]
+                        location["employee_name"] = employee["employee_name"]
+                        location["employee_email"] = employee["email_id"]
+                        location["employee_designation"] = employee["type_id"]
 
                         # Save the token into the database
-                        db2.insert_one({"s_token": s_token, "s_id": str(user["_id"]),
+                        db2.insert_one({"s_token": s_token, "s_id": str(location["_id"]),
                                         "created_at": datetime.now(pytz.utc),
                                         "local_time": dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                                         "expired_at": datetime.now(pytz.utc) + dt.timedelta(days=1)})
 
-                        response = {"status": 'success', "data": user, "message": "Login successful"}
+                        response = {"status": 'success', "data": location, "message": "Login successful"}
                         stop_and_check_mongo_status(conn)
                         return make_response(jsonify(response)), 200
 
@@ -250,10 +269,61 @@ class SuperLogin(MethodView):
             return make_response(jsonify(response)), 400
 
 
+class AssignSuperEmployee(MethodView):
+    @cross_origin(supports_credentials=True)
+    def post(self):
+        try:
+            start_and_check_mongo()
+            db = database_connect_mongo()
+            if db is not None:
+                db1 = db["super_location"]
+                db2 = db["super_employee"]
+                data = request.get_json()
+                s_l_id = data["s_l_id"]
+                s_e_id = data["s_e_id"]
+
+                if not s_l_id and s_e_id:
+                    response = {"status": 'val_error', "message": {"Details": ["Please enter all details"]}}
+                    stop_and_check_mongo_status(conn)
+                    return make_response(jsonify(response)), 400
+
+                find_s_l = db1.find_one({"_id": ObjectId(s_l_id), "status": "active"})
+                find_s_e = db2.find_one({"_id": ObjectId(s_e_id), "status": "active"})
+
+                if not find_s_l or not find_s_e:
+                    response = {"status": 'val_error', "message": {"Details": ["Please enter valid details"]}}
+                    stop_and_check_mongo_status(conn)
+                    return make_response(jsonify(response)), 400
+
+                else:
+                    db1.update_one({"_id": ObjectId(s_l_id)}, {"$set": {"emp_assign": "true"}})
+                    db2.update_one({"_id": ObjectId(s_e_id)}, {"$set": {"s_l_id": s_l_id}})
+                    db2.update_one({"_id": ObjectId(s_e_id)}, {"$set": {"loc_assign": "true"}})
+                    db1.update_one({"_id": ObjectId(s_l_id)}, {"$set": {"s_e_id": s_e_id}})
+                    response = {"status": 'success', "message": "Employee assigned successfully"}
+                    stop_and_check_mongo_status(conn)
+                    return make_response(jsonify(response)), 200
+
+            else:
+                response = {"status": 'val_error', "message": {"Details": ["Database connection failed"]}}
+                stop_and_check_mongo_status(conn)
+                return make_response(jsonify(response)), 400
+
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            response = {"status": 'val_error', "message": f'{str(e)}'}
+            stop_and_check_mongo_status(conn)
+            return make_response(jsonify(response)), 400
+
+
 s_emp_reg = SuperEmployeeRegistration.as_view('s_emp_reg_view')
 s_loc_reg = AddSuperLocation.as_view('s_loc_reg_view')
 s_login = SuperLogin.as_view('s_login_view')
+s_emp_assign = AssignSuperEmployee.as_view('s_emp_assign_view')
 
 super_employee_access.add_url_rule('/super_employee_access/registration', view_func=s_emp_reg, methods=['POST'])
 super_employee_access.add_url_rule('/super_employee_access/add_location', view_func=s_loc_reg, methods=['POST'])
 super_employee_access.add_url_rule('/super_employee_access/login', view_func=s_login, methods=['POST'])
+super_employee_access.add_url_rule('/super_employee_access/assign_super_employee', view_func=s_emp_assign,
+                                   methods=['POST'])
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from app import create_app
 
-app = [create_app(config_name='development'), create_app(config_name='production')]
-used_app = app[1]
+used_app = create_app(config_name='production')
 
 if __name__ == '__main__':
-    used_app.run(host="127.0.0.1", port=8000, debug=True)
+    used_app.run(host="127.0.0.1", port=8000)
+
+# if __name__ == '__main__':
+#     waitress.serve(app, host="127.0.0.1", port=8000)
+
+# if __name__ == '__main__':
+#     used_app.run(host="grain.learn-up.org", port=8000)
